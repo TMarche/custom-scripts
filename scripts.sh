c39f1524a7b300825dc4ea7d2b65fb78b0976a0a
Troy Marchesseault <tmarche@ncsu.edu>
2021-03-13 15:48:30 -0500

    Add 'vimtab' function

--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+## NOTE: Adjust this such that it can be pulled into config
 BOOK_LOC="/c/Users/tmarc/OneDrive/Desktop/Media/Books"
 
 findbooks () {
@@ -7,9 +8,18 @@ findbooks () {
         find ${BOOK_LOC} -type f -iname "*.pdf" | grep -iP "${SEARCH_STR}"
 }
 
+## In order for this command to work, the user must have the 'msedge'
+## program set up correctly
 openbooks () {
         local SEARCH_STR="${1:-}"
-        msedge $(findbooks "${SEARCH_STR}" | grep -vP "0.0.0.2") &> /dev/null &
+        msedge $(findbooks "${SEARCH_STR}") &> /dev/null &
 }
 
 win2lin () { f="${1/C://c}"; printf '%s\n' "${f//\\//}"; }
+
+vimtab () {
+	FILE=".vimrc";
+	TARGET="${HOME}/${FILE}";
+	sed -ri "s/(set[^/]*)(tab|shift)([^/]*)(=[[:digit:]]+)/\1\2\3=${1}/g" ${TARGET};
+	echo -e "$(grep -P "set.*?(tab|shift).*?=" ${TARGET})"
+}
